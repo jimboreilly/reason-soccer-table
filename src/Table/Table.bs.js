0895ec621e6399d9869ae21edad53ba75c1e2b7a
JamesMReilly <jimreilly28@gmail.com>
2020-07-08 17:00:39 -0500

    refactor row transformation into a single declarative expression to accomodate inconsistent styles applied to each td

--- src/Table/Table.bs.js
+++ src/Table/Table.bs.js
@@ -53,41 +53,29 @@ function transformRecordRow(position, r) {
   var style = {
     textAlign: "right"
   };
-  return React.createElement("tr", undefined, $$Array.of_list(List.map((function (value) {
-                        return React.createElement("td", {
-                                    style: style
-                                  }, value);
-                      }), {
-                      hd: String(position + 1 | 0),
-                      tl: {
-                        hd: r.name,
-                        tl: {
-                          hd: String((r.wins + r.draws | 0) + r.losses | 0),
-                          tl: {
-                            hd: String(r.wins),
-                            tl: {
-                              hd: String(r.draws),
-                              tl: {
-                                hd: String(r.losses),
-                                tl: {
-                                  hd: String(calculatePoints(r)),
-                                  tl: {
-                                    hd: String(r.goalsFor),
-                                    tl: {
-                                      hd: String(r.goalsAgainst),
-                                      tl: {
-                                        hd: String(r.goalsFor - r.goalsAgainst | 0),
-                                        tl: /* [] */0
-                                      }
-                                    }
-                                  }
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                    })));
+  var boldStyle = {
+    fontWeight: "bold",
+    textAlign: "right"
+  };
+  return React.createElement("tr", undefined, React.createElement("td", {
+                  style: style
+                }, String(position + 1 | 0)), React.createElement("td", undefined, r.name), React.createElement("td", {
+                  style: style
+                }, String((r.wins + r.draws | 0) + r.losses | 0)), React.createElement("td", {
+                  style: style
+                }, String(r.wins)), React.createElement("td", {
+                  style: style
+                }, String(r.draws)), React.createElement("td", {
+                  style: style
+                }, String(r.losses)), React.createElement("td", {
+                  style: boldStyle
+                }, String(calculatePoints(r))), React.createElement("td", {
+                  style: style
+                }, String(r.goalsFor)), React.createElement("td", {
+                  style: style
+                }, String(r.goalsAgainst)), React.createElement("td", {
+                  style: style
+                }, String(r.goalsFor - r.goalsAgainst | 0)));
 }
 
 function Table(Props) {
--- src/Table/Table.re
+++ src/Table/Table.re
@@ -24,23 +24,25 @@ let calculatePoints = (r: TeamRecord.t) => 3 * r.wins + r.draws;
 
 let transformRecordRow = (position, r: TeamRecord.t) => {
   let style = ReactDOMRe.Style.make(~textAlign="right", ());
-
+  let boldStyle =
+    ReactDOMRe.Style.make(~fontWeight="bold", ~textAlign="right", ());
   <tr>
-    {[
-       string_of_int(position + 1),
-       r.name,
-       string_of_int(r.wins + r.draws + r.losses),
-       string_of_int(r.wins),
-       string_of_int(r.draws),
-       string_of_int(r.losses),
-       string_of_int(calculatePoints(r)),
-       string_of_int(r.goalsFor),
-       string_of_int(r.goalsAgainst),
-       string_of_int(r.goalsFor - r.goalsAgainst),
-     ]
-     |> List.map(value => <td style> {React.string(value)} </td>)
-     |> Array.of_list
-     |> ReasonReact.array}
+    <td style> {React.string(string_of_int(position + 1))} </td>
+    <td> {React.string(r.name)} </td>
+    <td style>
+      {React.string(string_of_int(r.wins + r.draws + r.losses))}
+    </td>
+    <td style> {React.string(string_of_int(r.wins))} </td>
+    <td style> {React.string(string_of_int(r.draws))} </td>
+    <td style> {React.string(string_of_int(r.losses))} </td>
+    <td style=boldStyle>
+      {React.string(string_of_int(calculatePoints(r)))}
+    </td>
+    <td style> {React.string(string_of_int(r.goalsFor))} </td>
+    <td style> {React.string(string_of_int(r.goalsAgainst))} </td>
+    <td style>
+      {React.string(string_of_int(r.goalsFor - r.goalsAgainst))}
+    </td>
   </tr>;
 };
 
