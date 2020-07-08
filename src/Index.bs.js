277656160843dc5aecdc4fb616d2144f470ec5c1
JamesMReilly <jimreilly28@gmail.com>
2020-07-07 19:25:49 -0500

    move styles completely out of the raw dom manipulation

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -3,15 +3,7 @@
 var React = require("react");
 var ReactDOMRe = require("reason-react/src/legacy/ReactDOMRe.bs.js");
 var App$ReasonSoccerTable = require("./App/App.bs.js");
-var ExampleStyles$ReasonSoccerTable = require("./ExampleStyles.bs.js");
-
-var style = document.createElement("style");
-
-document.head.appendChild(style);
-
-style.innerHTML = ExampleStyles$ReasonSoccerTable.style;
 
 ReactDOMRe.renderToElementWithId(React.createElement(App$ReasonSoccerTable.make, {}), "app");
 
-exports.style = style;
-/* style Not a pure module */
+/*  Not a pure module */
